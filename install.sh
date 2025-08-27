056c2a2814a21f7cfe0043dafa6ae74b1a54b4c0
吴立伟 <eyouhi@gmail.com>
2025-08-27 10:00:05 +0800

    ..

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 sudo apt update -y && sudo  apt install cmake build-essential zip \
-     python3 python3-venv  libpq-dev  git curl wget zsh neovim -y
+    python3 python3-venv  libpq-dev  git curl wget zsh neovim -y
 
 # install go
 
@@ -12,23 +12,23 @@ sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.ta
 . ~/config/go/env
 
 go install github.com/go-delve/delve/cmd/dlv@latest
-
+go install github.com/charmbracelet/crush@latest
 
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh | sh
 
 rustup update
-rustup component add rls rust-analysis rust-src clippy
+rustup component add  rust-analysis rust-src clippy
 rustup target add wasm32-unknown-unknown
 rustup target add arm-unknown-linux-gnueabihf 
 rustup target add aarch64-unknown-linux-gnu
 
-cargo install bat pay-respects bore-cli bottom cargo-machete \
+cargo install bat pay-respects bore-cli bottom cargo-machete koji\
     eza  cargo-update cargo-zigbuild create-tauri-app hex-patch\
     dioxus-cli  fnm git-cliff git-delta ripgrep starship t-rec \
     procs sd tokei zellij just macchina skim  zoxide gping just-lsp \
-    trippy pik  hyperfine hurl tlrc cargo-deny cargo-nextest  \
-    cargo-autodd cargo-debugger presenterm du-dust codesnap-cli
+    trippy pik  hyperfine hurl tlrc cargo-deny cargo-nextest tukai \
+    cargo-autodd cargo-debugger presenterm du-dust codesnap-cli ast-grep
 
 cargo install --git https://github.com/bvaisvil/zenith.git
 
@@ -43,7 +43,8 @@ npm install --global serve
 # install python
 python3 -m venv ~/.venv
 source ~/.venv/bin/activate
-pip3 install -U pgcli mycli poetry
+pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+pip3 install -U pgcli mycli poetry 
 
 
 rm dev.zip  Justfile  README.md install.sh
